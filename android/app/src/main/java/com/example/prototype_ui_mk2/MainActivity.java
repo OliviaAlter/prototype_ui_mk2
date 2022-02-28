209ddc7d0eebf2fe685663ea455491dbe0bb2d78
MyouriAlter <innocentbystander0123@gmail.com>
2022-02-28 20:27:15 +0700

    Initial commit

---
+++ android/app/src/main/java/com/example/prototype_ui_mk2/MainActivity.java
@@ -0,0 +1,8 @@
+package com.example.prototype_ui_mk2;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+     super.onCreate(savedInstanceState);
+     GeneratedPluginRegistrant.registerWith(new FlutterEngine(this));
+}
